7a54f47476b8e0e18304d9fbee29b90d6252086c
Brian Ovalles <brianovalles@brians-macbook-pro.local>
2019-01-12 12:50:12 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Brian Ovalles on 1/12/19.
+//  Copyright © 2019 Brian Ovalles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
